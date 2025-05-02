fa3cad68925bc103a9a6853561e09792a8c63911
kramalmr <akram3107@student.abudzar.sch.id>
2025-05-02 11:02:23 +0700

    akram

--- kolaborasi-with-akram/src/App.jsx
+++ kolaborasi-with-akram/src/App.jsx
@@ -1,8 +1,10 @@
-export default function app () {
+export default function App () {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-center my-10">Ini Projec t Abduh</h1>
       <img src="/parid.jpg" alt="" className="w-1/2 mx-auto"/>
+      <h1 className="text-3xl font-bold underline text-center my-10">Ini Kolaborasi Akram</h1>
+      <img src="/akram.jpeg" alt="" className="w-1/2 mx-auto"/>
     </div>
   )
 }
